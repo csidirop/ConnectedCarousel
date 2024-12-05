8982bb057f40bdfb0f0419ee56ea019198d82913
Christos Sidiropoulos <dev@csidirop.de>
2024-12-05 14:18:32 +0000

    Update Carousel
    Signed-off-by: Christos Sidiropoulos <dev@csidirop.de>

--- views/public/carousel.php
+++ views/public/carousel.php
@@ -1,167 +1,81 @@
 <?php
-$captionPosition = isset($params['captionLocation'])
-    ? html_escape($params['captionLocation'])
-    : 'left';
-$showDesc = isset($params['showDescr'])
-    ? html_escape($params['showDescr'])
-    : 'false';
-$float = isset($params['float'])
-	? html_escape($params['float'])
-	: 'left';
-$width = isset($params['width'])
-	? html_escape($params['width'])
-	: '95%';
-$Nav = isset($params['noNav'])
-	? html_escape($params['noNav'])
-	: 'true';
-if ($Nav == 'true' || $configs['autoPlay'] == 'true'){
-	$shoArrows = 'false';
-}else{
-	$shoArrows = 'true'; 
-};
-if ($Nav == 'true'){
-	$setPos = 'block';
-}else{
-	$setPos = 'none';
-};
-if ($width != '100%'){
-	$tempwidth = '95%';
-}else{
-	$tempwidth = '100%';
-};
+$captionPosition = html_escape($params['captionLocation'] ?? 'left');
+$showDesc = filter_var($params['showDescr'] ?? 'false', FILTER_VALIDATE_BOOLEAN);
+$float = html_escape($params['float'] ?? 'left');
+$width = html_escape($params['width'] ?? '100%');
+$noNav = filter_var($params['noNav'] ?? 'true', FILTER_VALIDATE_BOOLEAN);
+$idSuffix = $id_suffix ?? 0;
+$carouselId = "connected-carousel-$idSuffix";
+$navId = !$noNav ? "connected-nav-$idSuffix" : null;
 ?>
 
-<div style="max-width:100%; max-height:100%; width:<?php echo $width;?>; float:<?php echo $float;?>; ">
-	<div class="carousel-navigation<?php echo $id_suffix;?>" style="max-width:100%; max-height:100%; width:<?php echo $tempwidth;?>; display:<?php echo $setPos;?>">
-		<?php foreach($items as $item): 
-			set_current_record('Item', $item);
-			if (metadata($item,'has files'))
-			{	?>
-				<?php echo files_for_item(
-				array(
-					'linkToFile' => false,
-					'imageSize' => 'square_thumbnail'
-				)
-			);?>
-			<?php }; ?>
-		<?php endforeach; ?>
-	</div>		
+<div id="<?= $carouselId ?>" class="connected-carousel" style="float: <?= $float ?>; width: <?= $width ?>;">
+    <div class="stage">
+        <?php foreach ($items as $item): ?>
+            <?php foreach ($item->Files as $file): ?>
+                <div class="image-container">
+                    <?php
+                    $img = file_image('fullsize', [], $file);
+                    echo link_to($file, 'show', $img, ['class' => 'fancybox', 'data-fancybox' => 'gallery']);
+                    ?>
+                    <?php if ($captionPosition !== 'none'): ?>
+                        <div class="caption" style="text-align: <?= $captionPosition ?>;">
+                            <?= metadata($file, ['Dublin Core', 'Description']) ?: metadata($item, ['Dublin Core', 'Description']) ?>
+                        </div>
+                    <?php endif; ?>
+                    <?php if ($showDesc): ?>
+                        <div class="description">
+                            <?= all_element_texts($item, ['show_empty_elements' => false]) ?>
+                        </div>
+                    <?php endif; ?>
+                </div>
+            <?php endforeach; ?>
+        <?php endforeach; ?>
+    </div>
 
-	<div class="carousel-stage<?php echo $id_suffix;?>" style="max-width:100%; max-height:100%; width:<?php echo $tempwidth;?>;" >
-		<?php foreach($items as $item):
-			set_current_record('Item', $item);
-			if (metadata($item,'has files'))
-			{ 
-				$itemFiles = $item->Files;
-				foreach($itemFiles as $itemfile):?>
-				<div>
-					<?php echo file_markup($itemfile,array('imageSize' => 'fullsize','linkToFile' => true, 'imgAttributes' => array('max-height'=>'100%','max-width'=>'100%','width'=>'100%', 'class'=>'full imgFile'.$id_suffix)));?>
-					<?php if ($captionPosition != 'none'){ ?>
-						
-					<p class="desc caption-<?php echo $captionPosition; ?>">			
-						<?php if (metadata($itemfile,array('Dublin Core','Title'))){
-							echo metadata($itemfile,array('Dublin Core','Title'));
-						}else{
-							echo html_escape(metadata($item, array('Dublin Core', 'Title'))),' ';    
-						}?>
-					</p>
-					<?php }?>
-								
-						<?php if ($showDesc == 'true'):?>
-								<div id="item-metadata">
-									<?php echo all_element_texts('item'); ?>
-								</div>
-						<?php endif; ?>	
-				</div>
-				<?php endforeach; ?>
-			<?php }; ?>
-		<?php endforeach; ?>
-	</div>
+        <div id="<?= $navId ?>" class="navigation">
+            <?php foreach ($items as $item): ?>
+                <?php foreach ($item->Files as $file): ?>
+                    <div class="thumbnail">
+                        <?= file_image('square_thumbnail', [], $file) ?>
+                    </div>
+                <?php endforeach; ?>
+            <?php endforeach; ?>
+        </div>
 </div>
-	
-<script type="text/javascript">
-	jQuery(document).ready(function(){
-		jQuery('.carousel-stage<?php echo $id_suffix;?>')
-		        .on('init', function(event, slick){
-					jQuery('img.imgFile<?php echo $id_suffix;?>')[0].onmouseover = (function() {
-					    var onmousestop = function() {
-					       jQuery('input.slick-next').css('display', 'none');
-					       jQuery('input.slick-prev').css('display', 'none');
-					    }, thread;
 
-				    return function() {
-				       jQuery('input.slick-next').css('display', 'block');
-				       jQuery('input.slick-prev').css('display', 'block');
-					        clearTimeout(thread);
-					        thread = setTimeout(onmousestop, 2000);
-					    };
-					})();
-		        });
-	jQuery('.carousel-navigation<?php echo $id_suffix;?>').slick({
-		centerPadding: '60px',
-	    asNavFor: '.carousel-stage<?php echo $id_suffix;?>',
-		accessibility:true,    
-		swipeToSlide: true,
-		variableWidth: false,
-		adaptiveHeight: false,
-		slidesToShow: <?php echo $configs['slidesToShow'];?>,
-		slidesToScroll: <?php echo $configs['slidesToScroll'];?>,
-	    centerMode: <?php echo $configs['centerMode'];?>,
-		autoplay: <?php echo $configs['autoPlay'];?>,
-		autoplaySpeed: <?php echo $configs['autoplaySpeed'];?>,
-	    focusOnSelect: <?php echo $configs['focusOnSelect'];?>,
-		arrows: <?php echo $configs['arrows'];?>,
-		prevArrow: '<input class="slick-prev" type="submit" value="&laquo;" />',
-		nextArrow: '<input class="slick-next" type="submit" value="&raquo;" />',
-	});
-	 jQuery('.carousel-stage<?php echo $id_suffix;?>').slick({
-	    slidesToShow: 1,
-	    slidesToScroll: 1,
-	    arrows: <?php echo $shoArrows;?>,
-	    fade: true,
-		centerMode: true,
-		variableWidth: false,
-		adaptiveHeight: true,
-	    asNavFor: '.carousel-navigation<?php echo $id_suffix;?>',
-		prevArrow: '<input class="slick-prev" type="submit" value="&laquo;" />',
-		nextArrow: '<input class="slick-next" type="submit" value="&raquo;" />',
-	});
-		
-	jQuery(".imgFile<?php echo $id_suffix;?>").parent()
-		.attr('rel', 'gallery<?php echo $id_suffix;?>')
-		.fancybox({
-	   		openEffect	: 'elastic',
-	    	closeEffect	: 'elastic',
-			fitToView: 'true',
-			arrows: 'true',
+<script>
+jQuery(document).ready(function($) {
+    var $carousel = $('#<?= $carouselId ?> .stage');
+    var navSelector = <?= !$noNav ? "'#$navId'" : 'null' ?>;
 
-	    	helpers : {
-	    		title : {
-	    			type : 'inside'
-	    		}
-	    	},
-			'beforeClose': function(current){
-				jQuery('.carousel-navigation<?php echo $id_suffix;?>').slick('slickGoTo',this.index);
-			}
-	});
-		
-	jQuery('.carousel-stage<?php echo $id_suffix;?>').on('afterChange',function(event, slick, currentSlide, nextSlide){
-			
-	jQuery('img.imgFile<?php echo $id_suffix;?>')[jQuery('.carousel-stage<?php echo $id_suffix;?>').slick('slickCurrentSlide')].onmouseover = (function() {
-		var onmousestop = function() {
-			jQuery('input.slick-next').css('display', 'none');
-		    jQuery('input.slick-prev').css('display', 'none');
-		}, thread;
+    // Initialize the main carousel
+    $carousel.slick({
+        slidesToShow: <?= json_encode($configs['slidesToShow']) ?>,
+        slidesToScroll: <?= json_encode($configs['slidesToScroll']) ?>,
+        centerMode: <?= json_encode(filter_var($configs['centerMode'], FILTER_VALIDATE_BOOLEAN)) ?>,
+        autoplay: <?= json_encode(filter_var($configs['autoPlay'], FILTER_VALIDATE_BOOLEAN)) ?>,
+        autoplaySpeed: <?= json_encode($configs['autoplaySpeed']) ?>,
+        focusOnSelect: <?= json_encode(filter_var($configs['focusOnSelect'], FILTER_VALIDATE_BOOLEAN)) ?>,
+        arrows: true, // Ensure this is true
+        asNavFor: navSelector
+    });
 
-	    return function() {
-	       jQuery('input.slick-next').css('display', 'block');
-	       jQuery('input.slick-prev').css('display', 'block');
-		        clearTimeout(thread);
-		        thread = setTimeout(onmousestop, 2000);
-		    };
-		})();
-		});
-		
-	});
+    // Initialize navigation carousel if enabled
+    if (navSelector) {
+        $(navSelector).slick({
+            slidesToShow: <?= json_encode($configs['slidesToShow']) ?>,
+            slidesToScroll: <?= json_encode($configs['slidesToScroll']) ?>,
+            asNavFor: $carousel,
+            focusOnSelect: true,
+            centerMode: <?= json_encode(filter_var($configs['centerMode'], FILTER_VALIDATE_BOOLEAN)) ?>
+        });
+    }
 
+    // Initialize Fancybox for image gallery
+	$('[data-fancybox="gallery"]').fancybox({
+		buttons: ['slideShow', 'fullScreen', 'thumbs', 'close'],
+		loop: true
+	});
+});
 </script>
